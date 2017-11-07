7aee8a034462cabd7bbfaa8b25b69a7a2e88d4e4
martinstuder <martin.studer@mirai-solutions.com>
2017-11-07 16:08:40 +0000

    include assembly with databricks spark-avro 4.0.0

--- R/dependencies.R
+++ R/dependencies.R
@@ -7,7 +7,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
         # TODO: check how to support multiple spark/scala versions
         # see for example https://github.com/javierluraschi/sparkhello/tree/master/inst/java
         # and https://github.com/javierluraschi/sparkhello/blob/master/R/dependencies.R
-        sprintf("java/spark-bigquery-assembly-%s.jar", sparkBigQueryVersion),
+        sprintf("java/spark-bigquery_2.11-%s.jar", sparkBigQueryVersion),
         package = "sparkbq"
       )
     ),
