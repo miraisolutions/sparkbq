1fabb70041fa925df88080d27c2538381c7c5fcc
Nicola Lambiase <nicola.lambiase@mirai-solutions.com>
2018-07-24 11:41:14 +0200

    Use spark-bigquery Spark package; update Spark version requirements

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,15 +1,12 @@
 spark_dependencies <- function(spark_version, scala_version, ...) {
   sparkBigQueryVersion = "0.1.0-SNAPSHOT"
-  if(spark_version != "2.2" || scala_version != "2.11") {
-    stop("This version of sparkbq currently only supports Spark 2.2 with Scala 2.11")
+  if(spark_version < "2.2" || scala_version != "2.11") {
+    stop("This version of sparkbq currently only supports Spark 2.2 or newer with Scala 2.11")
   }
   
   sparklyr::spark_dependency(
-    jars = c(
-      system.file(
-        sprintf("java/spark-bigquery_%s-%s.jar", scala_version, sparkBigQueryVersion),
-        package = "sparkbq"
-      )
+    packages = c(
+      sprintf("miraisolutions:spark-bigquery:%s-s_%s", sparkBigQueryVersion, scala_version)
     )
   )
 }
